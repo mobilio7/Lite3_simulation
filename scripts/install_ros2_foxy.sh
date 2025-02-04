2c446b9abad4c872a9e7e1b7adbf6aea1011200d
jslee <108033045+mobilio7@users.noreply.github.com>
2025-02-04 09:04:04 +0900

    Create install_ros2_foxy.sh

---
+++ scripts/install_ros2_foxy.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+sudo apt update
+
+sudo apt install locales -y
+sudo locale-gen en_US en_US.UTF-8
+sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
+export LANG=en_US.UTF-8
+
+sudo apt install software-properties-common -y
+sudo add-apt-repository universe
+
+sudo apt update && sudo apt install curl -y
+sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
+
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
+
+sudo apt update
+sudo apt upgrade -y
+
+sudo apt install ros-foxy-desktop python3-argcomplete -y
+sudo apt install python3-colcon-common-extensions -y
+
+# Install ROS1 Bridge
+sudo apt install ros-foxy-ros1-bridge -y
